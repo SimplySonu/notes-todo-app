07d68fe2e02096e1feb9edac1d7edc013f83e1d6
SimplySonu <bsonu.saw@gmail.com>
2022-03-06 17:22:34 +0530

    Initial Commit

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,8 +2,7 @@ import React from "react";
 import notesSvg from "../images/notes.svg";
 import todoSvg from "../images/todo.svg";
 import { Link } from "react-router-dom";
-// git config --global user.email "you@example.com"
-//   git config --global user.name "Your Name"
+
 export default function SideBar() {
 	return (
 		<div className='sidebar-container'>
